8c827a91e1d0a570e0bb8ece11f76b4c8040f51a
Benjamin Quinn <benjamin.quinn92@gmail.com>
2025-03-21 10:15:29 -0400

    Remove public dir

--- public/js/plain_text_reader.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  const readers = document.getElementsByClassName("plain-text-reader");
-  for (const r of readers) {
-    const src = r.dataset.contentSrc;
-    const res = await fetch(src);
-    const txt = await res.text();
-    r.value = txt;
-  }
-});
