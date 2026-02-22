3d8d84381c33611afff27f81f4b5c955c828509e
Benjamin Quinn <benjamin.quinn92@gmail.com>
2026-02-22 17:49:25 -0500

    JS const

--- static/js/content/blog/twenties_countdown.js
+++ static/js/content/blog/twenties_countdown.js
@@ -1,28 +1,27 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
-  let countdownEl = document.getElementById("countdown");
+  const countdownEl = document.getElementById("countdown");
 
-  let countDownDate = new Date("May 16, 2026 00:00:00").getTime();
+  const countDownDate = new Date("May 16, 2026 00:00:00").getTime();
 
   // Returns `true` if there's still time remaining.
   let setCount = () => {
-    let now = new Date().getTime();
+    const now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    let distance = countDownDate - now;
+    const distance = countDownDate - now;
 
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    countdownEl.innerHTML = days + "d " + hours + "h "
-      + minutes + "m " + seconds + "s ";
+    countdownEl.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     return distance >= 0;
   };
 
-  let x = setInterval(() => {
+  const x = setInterval(() => {
     if (!setCount()) {
       clearInterval(x);
       countdownEl.innerHTML = "Over!!!";
@@ -31,3 +30,4 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   setCount();
 });
+
